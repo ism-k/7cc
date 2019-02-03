5ba0c9d70dd099ae7e4fd6398d87e1f5a3553dfb
Izumu KUSUNOKI <kusunoki.iz@dsp.cse.kyutech.ac.jp>
2019-02-03 19:30:03 +0900

    トークナイザの実装
    スペースを取り数字，演算子ごとにトークンに格納する構造Tokenを作成

--- 7cc.c
+++ 7cc.c
@@ -1,5 +1,73 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Values represent type of token
+enum
+{
+    TK_NUM = 256, // int
+    TK_EOF,       // End of Input
+};
+
+// Type
+typedef struct
+{
+    int  ty;     // Type of token
+    int  val;    // Value
+    char *input; // String of input (for error message)
+} Token;
+
+Token tokens[100];
+
+// Divide p-strings by each tokens
+void tokenize(char *p)
+{
+    int i = 0;
+
+    while (*p)
+    {
+        // Skip white space
+        if (isspace(*p)) 
+        {
+            p++;
+            continue;
+        }
+        
+        // Operator
+        if (*p == '+' || *p == '-') 
+        {
+            tokens[i].ty    = *p;
+            tokens[i].input =  p;
+            i++;
+            p++;
+            continue;
+        }
+
+        // Integer
+        if (isdigit(*p)) 
+        {
+            tokens[i].ty    = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val   = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+        
+        fprintf(stderr, "Unable to tokenize: %s\n", p);
+        exit(1);
+    }
+
+    tokens[i].ty    = TK_EOF;
+    tokens[i].input = p;
+}
+
+// Report Error
+void error(int i)
+{
+    fprintf(stderr, "Unexpected Token: %s\n", tokens[i].input);
+    exit(1);
+}
 
 int main(int argc, char **argv)
 {
@@ -9,31 +77,44 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    char *p = argv[1];
+    tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p)
+    // tokens[0] must be int
+    if (tokens[0].ty != TK_NUM) 
+        error(0);
+    
+    printf("    mov rax, %d\n", tokens[0].val);
+
+    int i = 1;
+    while (tokens[i].ty != TK_EOF)
     {
-        if (*p == '+') 
+        if (tokens[i].ty == '+') 
         {
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            i++;
+            if (tokens[i].ty != TK_NUM) 
+                error(i);
+            
+            printf("    add rax, %d\n", tokens[i].val);
+            i++;
             continue;
         }
 
-        if (*p == '-') 
+        if (tokens[i].ty == '-') 
         {
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            i++;
+            if (tokens[i].ty != TK_NUM) 
+                error(i);
+
+            printf("    sub rax, %d\n", tokens[i].val);
+            i++;
             continue;
         }
 
-        fprintf(stderr, "Unexpected string: '%c'\n", *p);
-        return 1;
+        error(i);
     }
 
     printf("    ret\n");
